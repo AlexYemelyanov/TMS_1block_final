7129a65c30da8237590e6c464d35b2d856822f3f
Alexey <a.yemelyanovfe@gmail.com>
2023-06-22 01:35:22 +0300

    end layout

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,10 @@ let acc = document.getElementsByClassName("skillset__accordion");
 let i;
 let prev = document.querySelector(".carousel__prev");
 let next = document.querySelector(".carousel__next");
+let prevCircle = document.querySelector(".carousel__prev-circle");
+let nextCircle = document.querySelector(".carousel__next-circle");
+
+
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener('click', function() {
@@ -54,6 +58,41 @@ prev.addEventListener('click', function (e) {
 
 next.addEventListener('click', function (e) {
     plusSlides(1)
+});
+
+
+
+let slideId = 1;
+showSlds(slideId);
+
+// Next/previous controls
+function plusSlds(n) {
+  showSlds(slideId += n);
+}
+
+// Thumbnail image controls
+function currentSld(n) {
+  showSlds(slideId = n);
+}
+
+function showSlds(n) {
+  let i;
+  let slds = document.getElementsByClassName("carousel__slide-circle");
+  if (n > slds.length) {slideId = 1}
+  if (n < 1) {slideId = slds.length}
+  for (i = 0; i < slds.length; i++) {
+      slds[i].style.display = "none";
+  }
+  slds[slideId-1].style.display = "flex";
+  
+}
+
+prevCircle.addEventListener('click', function (e) {
+    plusSlds(-1)
+});
+
+nextCircle.addEventListener('click', function (e) {
+    plusSlds(1)
 });
 
 
